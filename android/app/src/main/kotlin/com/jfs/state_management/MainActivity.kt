af834a04529b6c8c59e7691f974d1bec4cf307c5
Junia Firdaus <firdausjunia@gmail.com>
2022-04-07 06:03:38 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/jfs/state_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jfs.state_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/provider/done_module_provider.dart';
+import 'package:state_management/ui/module_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => DoneModuleProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ModulePage(),
+      ),
+    );
+  }
+}
